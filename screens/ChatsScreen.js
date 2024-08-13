4bef07cd82ebfa347ffa0d8b58062f11dcc55088
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-08-13 10:52:10 +0600

    chatlist fnd added with backend endpoint

--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserType } from '../UserContext';
+import { useNavigation } from '@react-navigation/native';
+import UserChat from '../component/UserChat';
 
 const ChatsScreen = () => {
-  return (
-    <View>
-      <Text>ChatsScreen</Text>
-    </View>
-  )
+    const [acceptedFriends, setAcceptedFriends] = useState([]);
+    const { userId, setUserId } = useContext(UserType);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+      const acceptedFriendsList = async () => {
+        try {
+          const response = await fetch(
+            `http://192.168.6.218:5000/accepted-friends/${userId}`
+          );
+          const data = await response.json();
+  
+          if (response.ok) {
+            setAcceptedFriends(data);
+          }
+        } catch (error) {
+          console.log("error showing the accepted friends", error);
+        }
+      };
+  
+      acceptedFriendsList();
+    }, []);
+
+    console.log("friends", acceptedFriends);
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <Pressable>
+                {acceptedFriends.map((item, index) => (
+                    <UserChat key={index} item={item} />
+                ))}
+            </Pressable>
+        </ScrollView>
+    );
 }
 
-export default ChatsScreen
+export default ChatsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
